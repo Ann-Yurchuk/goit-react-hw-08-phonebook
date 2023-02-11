e13106bed7fe4210e5cb1c5fe6d41985b91c264e
Ann-Yurchuk <105579383+Ann-Yurchuk@users.noreply.github.com>
2023-02-11 12:34:33 +0200

    update

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 export const Main = styled.div`
   display: grid;
   gap: 16px;
-  max-width: 800px;
+  max-width: 1800px;
   margin: 0 auto;
   padding: 16px;
   justify-content: center;
